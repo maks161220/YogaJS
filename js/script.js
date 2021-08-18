2a37842092b81f305bdacbe23d18ce25572ea278
Maks Marchuk <max.hudykovsky@gmail.com>
2021-08-18 15:02:18 +0300

    Slider

--- js/script.js
+++ js/script.js
@@ -210,4 +210,50 @@ window.addEventListener('DOMContentLoaded', function() {    //пишемо за�
                                 .catch(() => showMessage.innerHTML = message.fail)
                                 .then(() => clearInput());
     });
+
+    //Slider
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrapper = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if(n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if(n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlides(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', () => plusSlides(-1));
+
+    next.addEventListener('click', () => plusSlides(1));
+
+    dotsWrapper.addEventListener('click', function(e){
+        for(let i = 0; i < dots.length + 1; i++) {
+            if (e.target.classList.contains('dot') && e.target == dots[i -1 ]){
+                currentSlides(i);
+            }
+        }
+    });
 });
